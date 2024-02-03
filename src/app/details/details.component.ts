d6fd268cf449defccb3c9cad2ca900096d7da6e7
Manoel <125422009+ManoelMorais@users.noreply.github.com>
2024-02-03 18:10:21 -0300

    Primeiro Contato com Angular

--- src/app/details/details.component.ts
+++ src/app/details/details.component.ts
@@ -28,13 +28,13 @@ import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
     <section class="listing-apply">
       <h2 class="section-heading">Apply now to live here</h2>
       <form [formGroup]="applyForm" (submit)="submitApplication()">
-        <label for="first-name">First Name</label>
+        <label for="first-name">First Name:</label>
         <input id="first-name" type="text" formControlName="firstName">
 
-        <label for="last-name">Last Name</label>
+        <label for="last-name">Last Name:</label>
         <input id="last-name" type="text" formControlName="lastName">
 
-        <label for="email">Email</label>
+        <label for="email">Email:</label>
         <input id="email" type="email" formControlName="email">
         <button type="submit" class="primary">Apply now</button>
       </form>
